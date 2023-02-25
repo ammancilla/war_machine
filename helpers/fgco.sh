ab158fe69054061272b38ee1a4004ae0d3c05878
Alfonso Manuel <almancill@gmail.com>
2023-02-25 13:23:11 +0100

    Add fgco5 helper function

--- helpers/fgco.sh
+++ helpers/fgco.sh
@@ -3,7 +3,14 @@
 #
 # Dinamically change between git branches using fzf.
 #
-
-fgco() {
-  git branch -vv | fzf +m --query="$1" | awk '{print $1}' | xargs git checkout
+fgco () {
+  limit="${2:-100}"
+  git branch --sort=-committerdate --format='%(refname:short)|%(committerdate:relative)|%(subject)' | \
+    column -ts '|' | \
+    head -n "$limit" | \
+    fzf +m --query="$1" | \
+    awk '{print $1}' | \
+    xargs git checkout
 }
+
+fgco5 () { fgco "$1" 5; }
